50841ee284dfdc987a2368f3aaa562e09de29423
Mol <375753904@qq.com>
2023-12-29 23:53:28 +0900

    vue 透传

--- vue/vue_practice/03_refdemo/refDemo/src/App.vue
+++ vue/vue_practice/03_refdemo/refDemo/src/App.vue
@@ -14,8 +14,8 @@
 <template>
   <!-- 想把标签里的东西设置为变量就只能用v-bind不能插值 -->
   <img v-bind:src=toiletSrc height = "800" alt="tokyo toilet"/>
-  <button @click="changeTheToilet">点击换厕所</button>
-  <div v-bind="vbindTest"></div>
+  <!-- <button @click="changeTheToilet">点击换厕所</button> -->
+  <!-- <div v-bind="vbindTest"></div> -->
 
 </template>
 
---
+++ vue/vue_practice/04_Demo/vue-project/src/App.vue
@@ -0,0 +1,65 @@
+<script setup>
+  import {ref} from 'vue'
+  const teamActiver = ref(0)
+
+</script>
+
+
+<template>
+  <!-- 最外层的容器 -->
+  <div class="top-wrapper">
+
+
+    <header class="top-header">
+      <div @click="teamActiver=0" class="header-button" :class="{ active: teamActiver === 0}">
+        热门球员
+        <!-- <button class="球队"></button>
+        <button class="球员"></button> -->
+      </div>
+      <div @click="teamActiver=1" class="header-button" :class="{ active: teamActiver === 1}">
+        热门球队
+      </div>
+    </header>
+
+    <!-- 信息主体 -->
+    <div class="main" style="color: #fff">
+      <!-- 这里显示的内容应该和上面选项卡的选择一致 -->
+      <div v-show="teamActiver === 0">球员</div>
+      <div v-show="teamActiver === 1">球队</div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.top-wrapper{
+  /* 固定大小 */
+  box-sizing: border-box;
+  background-color:#352ce0;
+  width: 800px;
+  padding:20px;
+}
+
+.top-header{
+  display: flex;
+  border-radius: 15px;
+  overflow: hidden ;
+}
+
+.header-button{
+  background-color: aquamarine;
+  flex:auto;
+  font-size: 30px;
+  padding: 10px;
+  width: 380px;
+  cursor: pointer;
+  transition: 0.2s;
+}
+
+.active{
+  background-color: #9a2020;
+  color:#fff;
+}
+
+.header-button:not(.active):hover {
+  color: #9a2020;
+}
+</style>
